2336cdf85684b0e4fac61bc55e941a7d43fd5974
Loïc <loic.etheve@lecole-ldlc.com>
2021-09-20 15:58:24 +0200

    Création de la vue des produits + vue unique des produits. Création du système de filtrage

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Classe\Search;
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -17,6 +18,31 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    /**
+     * Requete qui recup les produits en fonction des filtres
+     */
+    public function findWithSearch(Search $search){
+        $query = $this
+            ->createQueryBuilder('p') // on crée une query avec la table produit, représentée par un p
+            ->select('c','p') // on séléctionne c pour catégorie et p pour product
+            ->join('p.category','c'); // on fait la jointure entre le produit de la catégorie et la categ
+
+            if(!empty($search->categories)){
+                $query = $query
+                    ->andWhere('c.id IN (:categories)')
+                    ->setParameter('categories',$search->categories);
+            }
+
+            if (!empty($search->string)) {
+                $query = $query
+                    ->andWhere('p.Name LIKE :string')
+                    ->setParameter('string', "%{$search->string}%");
+            }
+
+
+        return $query->getQuery()->getResult();
     }
 
     // /**
