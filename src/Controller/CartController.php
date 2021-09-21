7b84e775eea08c957abffd5545da3d4748b84e3b
Loïc <loic.etheve@lecole-ldlc.com>
2021-09-21 15:35:06 +0200

    Ajout de la vue du panier + ajout/diminution de la quantité et suppresion d'un article

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,17 +3,29 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+
+    private $entityManagaer;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManagaer = $entityManager;
+    }
+
     #[Route('/mon-panier', name: 'cart')]
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+
+        return $this->render('cart/index.html.twig',[
+            'cart' => $cart->getFull()
+        ]);
     }
 
     #[Route('/cart/add/{id}', name: 'add_to_cart')]
@@ -28,5 +40,19 @@ class CartController extends AbstractController
     {
         $cart->remove();
         return $this->redirectToRoute('products');
+    }
+
+    #[Route('/cart/delete{id}', name: 'delete_to_cart')]
+    public function delete(Cart $cart, $id): Response
+    {
+        $cart->delete($id);
+        return $this->redirectToRoute('cart');
+    }
+
+    #[Route('/cart/decrease{id}', name: 'decrease_to_cart')]
+    public function decrease(Cart $cart, $id): Response
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('cart');
     }
 }
